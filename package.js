2251a78f52e696d2c717abb07f3edee21606b2c2
Xingchen Hong <hello@xc-h.net>
2015-12-04 17:13:22 -0600

    Add git info.

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Wrapped amqplib callback API.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Zodiase/meteor-amqplib-callback.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
